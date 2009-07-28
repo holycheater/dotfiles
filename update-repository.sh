f42d02b9e113960650d4bc1decc7c7f098b56ed7
Alexander Saltykov <holy.cheater@gmail.com>
2009-07-28 18:37:05 +0600

    update-repository.sh: update ~/.vim dir

--- update-repository.sh
+++ update-repository.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+# vim:noet:sw=4:ts=4
 
-FILES="gvimrc vimrc bash_profile dircolors zshrc screenrc Xdefaults pbuilderrc"
+FILES="gvimrc vimrc bash_profile dircolors zshrc screenrc Xdefaults pbuilderrc vim"
 
 for f in $FILES; do
 	fullpath="${HOME}/.${f}"
 	echo $fullpath
 	if [ -f $fullpath ]; then
 		cp $fullpath $f
+	elif [ -d $fullpath ]; then
+		rm -r $f
+		cp -a $fullpath $f
 	fi
 done
